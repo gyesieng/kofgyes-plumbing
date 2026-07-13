04af4b45fd8401656160fcc05a862e82d857f6d8
gyesieng <gyesi1998@gmail.com>
2026-07-13 11:30:42 +0000

    Update sw.js
    Add service worker

--- sw.js
+++ sw.js
@@ -22,3 +22,29 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+const CACHE_NAME = "kofgyes-plumbing-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./logo.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
