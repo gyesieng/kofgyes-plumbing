64f5cc63f230a7d0bcecad8d2d857fa5f534c1f0
gyesieng <gyesi1998@gmail.com>
2026-07-14 14:29:21 +0000

    Fix service worker and add PWA icons

--- sw.js
+++ sw.js
@@ -1,27 +1,3 @@
-const CACHE_NAME = "kofgyes-v1";
-
-const urlsToCache = [
-  "/kofgyes-plumbing/",
-  "/kofgyes-plumbing/index.html",
-  "/kofgyes-plumbing/style.css",
-  "/kofgyes-plumbing/script.js",
-  "/kofgyes-plumbing/logo.jpg",
-  "/kofgyes-plumbing/manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
 const CACHE_NAME = "kofgyes-plumbing-v1";
 
 const urlsToCache = [
@@ -30,7 +6,9 @@ const urlsToCache = [
   "./style.css",
   "./script.js",
   "./manifest.json",
-  "./logo.jpg"
+  "./logo.jpg",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
